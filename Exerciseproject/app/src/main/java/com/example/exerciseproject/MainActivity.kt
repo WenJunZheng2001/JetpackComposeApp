befe2df15a803771b044dfed842e929d1e0e386d
Wen Jun Zheng Giacomo <wenjunzheng2001@gmail.com>
2026-02-15 23:59:13 +0100

    aggiunto progetto android in jetpack compose

---
+++ Exerciseproject/app/src/main/java/com/example/exerciseproject/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.exerciseproject
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.exerciseproject.ui.theme.ExerciseProjectTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ExerciseProjectTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title= {Text("Hello")},
+                            actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                            } })},
+                    ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding),
+                        listOf("Hi", "Me", "Love you adsadasfagasdsadasd", "Me", "Love you adsadasfagasdsadasd")
+                    )
+                }
+            }
+        }
+    }
+}
+
+fun OnClickButton (){
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier, number: List<String>) {
+    LazyColumn(modifier = modifier) {
+        items(7){ index ->
+            Text("Hello " + index)
+        }
+    }
+}
+
+
+
